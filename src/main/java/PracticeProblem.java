ccd08dfd14998c286944b70a2400906158e395ce
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-03-31 18:04:19 +0000

    commit tower of hanoi real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,18 @@ public class PracticeProblem {
 
 	}
 
+	public static int towerOfHanoi (int num) {
+		if (num < 3) {
+			return -1;
+		}
+		return realTowerOfHanoi(num);
+	}
+	
+	public static int realTowerOfHanoi (int num) {
+		if (num == 1) {
+			return 1;
+		}
+		return 1 + 2*realTowerOfHanoi(num-1);
+	}
+
 }
